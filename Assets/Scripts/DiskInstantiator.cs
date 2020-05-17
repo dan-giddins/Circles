39408bd861815891e2d251e03e2d729c0c55353c
Dan Giddins <daniel.giddins@nottingham.ac.uk>
2020-05-17 16:45:23 +0100

    also scale

--- Assets/Scripts/DiskInstantiator.cs
+++ Assets/Scripts/DiskInstantiator.cs
@@ -11,41 +11,42 @@ public class DiskInstantiator : MonoBehaviour
 	void Start()
 	{
 		Generate6(new Vector3(0, 0, 0));
-		Generate14(new Vector3(2, 0, 0));
-
+		//Generate14(new Vector3(2, 0, 0));
 	}
 
-	private void Generate14(Vector3 vector3)
+	private void Generate14(Vector3 centre)
 	{
 		throw new NotImplementedException();
 	}
 
-	// Update is called once per frame
-	void Update()
-	{
-
-	}
-
 	private void Generate6(Vector3 centre)
 	{
-		Instantiate(DiskObject, Position(centre, 0.5f, 0, 0), Quaternion.Euler(new Vector3(0, 90, 0)));
-		Instantiate(DiskObject, Position(centre, -0.5f, 0, 0), Quaternion.Euler(new Vector3(0, 90, 0)));
-		Instantiate(DiskObject, Position(centre, 0, 0.5f, 0), Quaternion.Euler(new Vector3(90, 0, 0)));
-		Instantiate(DiskObject, Position(centre, 0, -0.5f, 0), Quaternion.Euler(new Vector3(90, 0, 0)));
-		Instantiate(DiskObject, Position(centre, 0, 0, 0.5f), Quaternion.Euler(new Vector3(0, 0, 0)));
-		Instantiate(DiskObject, Position(centre, 0, 0, -0.5f), Quaternion.Euler(new Vector3(0, 0, 0)));
+		InstantiateAndSize(centre, 0.5f, 0, 0, 0, 90, 0, 1);
+		InstantiateAndSize(centre, -0.5f, 0, 0, 0, 90, 0, 1);
+		InstantiateAndSize(centre, 0, 0.5f, 0, 90, 0, 0, 1);
+		InstantiateAndSize(centre, 0, -0.5f, 0, 90, 0, 0, 1);
+		InstantiateAndSize(centre, 0, 0, 0.5f, 0, 0, 0, 1);
+		InstantiateAndSize(centre, 0, 0, -0.5f, 0, 0, 0, 1);
+	}
+
+	private void InstantiateAndSize(
+		Vector3 centre,
+		float xPos,
+		float yPos,
+		float zPos,
+		float xRot,
+		float yRot,
+		float zRot,
+		float scale)
+	{
+		Instantiate(
+			DiskObject,
+			Position(centre, xPos, yPos, zPos),
+			Quaternion.Euler(new Vector3(xRot, yRot, zRot)))
+			.transform.localScale *= scale;
 	}
 
 	private Vector3 Position(Vector3 centre, float x, float y, float z) =>
 		centre + new Vector3(x, y, z);
 
-	private void RotateDisk(GameObject disk, Vector3 rotation) =>
-		disk.transform.Rotate(rotation);
-
-	private GameObject CreateAndMoveDisk(Vector3 centre, Vector3 offset, float size)
-	{
-		var disk = Instantiate(DiskObject);
-		disk.transform.localPosition = centre + offset;
-		return disk;
-	}
 }
