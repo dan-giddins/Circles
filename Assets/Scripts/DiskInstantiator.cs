060ba6bc817b3d88b7d17c4e1a17a1a0693b71db
Dan Giddins <daniel.giddins@nottingham.ac.uk>
2020-05-25 13:17:13 +0100

    start work on doing this better

--- Assets/Scripts/DiskInstantiator.cs
+++ Assets/Scripts/DiskInstantiator.cs
@@ -18,51 +18,40 @@ public class DiskInstantiator : MonoBehaviour
 	{
 		const float scale = 1;
 		// 6
-		InstantiateAndScale(centre, 1, 0, 0, 0, 90, 0, scale);
-		InstantiateAndScale(centre, -1, 0, 0, 0, 90, 0, scale);
-		InstantiateAndScale(centre, 0, 1, 0, 90, 0, 0, scale);
-		InstantiateAndScale(centre, 0, -1, 0, 90, 0, 0, scale);
-		InstantiateAndScale(centre, 0, 0, 1, 0, 0, 0, scale);
-		InstantiateAndScale(centre, 0, 0, -1, 0, 0, 0, scale);
-		// 8
-		InstantiateAndScale(centre, GetPos(45), GetPos(45), GetPos(45), -45, 45, 0, scale);
 	}
 
 	private float GetPos(float angle) =>
-		(float) Math.Pow(Math.Sin(ConvertToRadians(angle)),1.5);
+		(float)Math.Pow(Math.Sin(ConvertToRadians(angle)), 1.5);
 
 	public double ConvertToRadians(float angle) =>
 		Math.PI / 180 * angle;
 
-	private void Generate6(Vector3 centre)
+	private void Generate6(Vector3 position)
 	{
 		const float scale = 2;
-		InstantiateAndScale(centre, 1, 0, 0, 0, 90, 0, scale);
-		InstantiateAndScale(centre, -1, 0, 0, 0, 90, 0, scale);
-		InstantiateAndScale(centre, 0, 1, 0, 90, 0, 0, scale);
-		InstantiateAndScale(centre, 0, -1, 0, 90, 0, 0, scale);
-		InstantiateAndScale(centre, 0, 0, 1, 0, 0, 0, scale);
-		InstantiateAndScale(centre, 0, 0, -1, 0, 0, 0, scale);
+		var sphere = Instantiate(new GameObject(), position, new Quaternion());
+		sphere.name = "6Sphere";
+		CreateFace(sphere, 0, 0, scale, 0);
+		CreateFace(sphere, 0, 90, scale, 0);
+		CreateFace(sphere, 0, 180, scale, 0);
+		CreateFace(sphere, 0, 270, scale, 0);
+		CreateFace(sphere, 90, 0, scale, 0);
+		CreateFace(sphere, 270, 0, scale, 0);
 	}
 
-	private void InstantiateAndScale(
-		Vector3 centre,
-		float xPos,
-		float yPos,
-		float zPos,
+	private void CreateFace(
+		GameObject sphere,
 		float xRot,
 		float yRot,
-		float zRot,
-		float scale)
+		float scale, 
+		int i)
 	{
-		Instantiate(
-			DiskObject,
-			Position(centre, xPos, yPos, zPos),
-			Quaternion.Euler(new Vector3(xRot, yRot, zRot)))
-			.transform.localScale *= scale;
+		// Create parent
+		var parent = Instantiate(new GameObject(), new Vector3(), Quaternion.Euler(new Vector3(xRot, yRot)), sphere.transform);
+		parent.name = $"{sphere.name}_{i}_parent";
+		// Create disk
+		var disk = Instantiate(DiskObject, new Vector3(0, 0, 1), new Quaternion(), parent.transform);
+		disk.transform.localScale *= scale;
+		disk.name = $"{sphere.name}_{i}_disk";
 	}
-
-	private Vector3 Position(Vector3 centre, float x, float y, float z) =>
-		centre + new Vector3(x, y, z);
-
 }
